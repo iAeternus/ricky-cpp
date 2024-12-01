723d6081116edda18d9da707de147458c3209917
Ricky <w_ziwei2004@163.com>
2024-12-01 23:45:00 +0800

    commit

--- include/fake/fake.hpp
+++
@@ -1,6 +0,0 @@
-#ifndef FAKE_HPP
-#define FAKE_HPP
-
-void fake_function();
-
-#endif // FAKE_HPP
--- src/fake/fake.cpp
+++
@@ -1,7 +0,0 @@
-#include "fake.hpp"
-
-#include <iostream>
-
-void fake_function() {
-    std::cout << "This is a fake function!" << std::endl;
-}
