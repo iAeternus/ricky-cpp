9717cfbaec063b5eb4835d3f18d9e2967fb34bea
Ricky <w_ziwei2004@163.com>
2025-02-10 23:46:30 +0800

    Return value after

--- include/hash.hpp
+++ include/hash.hpp
@@ -12,17 +12,17 @@
 namespace my {
 
 template <Hashable K>
-inline hash_t my_hash_impl(const K& key, std::true_type) {
+def my_hash_impl(const K& key, std::true_type)->hash_t {
     return key.__hash__();
 }
 
 template <Hashable K>
-inline hash_t my_hash_impl(const K& key, std::false_type) {
+def my_hash_impl(const K& key, std::false_type)->hash_t {
     return std::hash<K>{}(key);
 }
 
 template <Hashable K>
-inline hash_t my_hash(const K& key) {
+def my_hash(const K& key)->hash_t {
     return my_hash_impl(key, std::bool_constant<MyLikeHashable<K>>{});
 }
 
@@ -37,7 +37,7 @@ inline hash_t my_hash(const K& key) {
  *            该数组至少应有4个字节的长度
  * @return 解码后的32位无符号整数
  */
-inline u32 decode_fixed32(const char* ptr) {
+def decode_fixed32(const char* ptr)->u32 {
     const u8* buffer = reinterpret_cast<const u8*>(ptr);
     return (static_cast<u32>(buffer[0]) | (static_cast<u32>(buffer[1]) << 8) | (static_cast<u32>(buffer[2]) << 16) | (static_cast<u32>(buffer[3]) << 24));
 }
@@ -63,7 +63,7 @@ inline u32 decode_fixed32(const char* ptr) {
  * @param seed 可选的种子值，用于初始化哈希计算。默认为0xbc9f1d34
  * @return 计算得到的哈希值
  */
-inline hash_t bytes_hash(const char* data, size_t n, u32 seed = 0xbc9f1d34) {
+def bytes_hash(const char* data, size_t n, u32 seed = 0xbc9f1d34)->hash_t {
     constexpr hash_t m = 0xc6a4a793;
     constexpr hash_t r = 24;
     const char* end = data + n;
