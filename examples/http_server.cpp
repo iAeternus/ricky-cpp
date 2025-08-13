1bbc9712464ec0ce42c29a7861b5dcedb0f1c45d
Ricky <w_ziwei2004@163.com>
2025-08-13 11:27:55 +0800

    example目录更名为examples

--- examples/http_server.cpp
+++ examples/http_server.cpp
@@ -18,26 +18,26 @@ void example_http_server() {
     net::HttpServer server(IP, PORT, NUM_OF_THREADS);
     io::Log::set_level(io::Log::LogLevel::TRACE_);
 
-    /*
-    静态文件服务
-    http://127.0.0.1:8080/static
-    */
+    /**
+     * 静态文件服务
+     * http://127.0.0.1:8080/static
+     */
     server.static_file("/static", STATIC_FILE_PATH, 3600);
 
-    /*
-    首页
-    http://127.0.0.1:8080/
-    */
+    /**
+     * 首页
+     * http://127.0.0.1:8080/
+     */
     server.get("/", [](const net::HttpRequest& req) {
         net::HttpResponse resp;
         resp.set_body("<h1>Welcome to Ricky-Cpp HTTP Server</h1>", "text/html");
         return resp;
     });
 
-    /*
-    问候页面
-    http://127.0.0.1:8080/hello?name=Ricky
-    */
+    /**
+     * 问候页面
+     * http://127.0.0.1:8080/hello?name=Ricky
+     */
     server.get("/hello", [](const net::HttpRequest& req) {
         net::HttpResponse resp;
         const auto name = req.query_params.get_or_default("name"_s, "Guest");
@@ -45,10 +45,10 @@ void example_http_server() {
         return resp;
     });
 
-    /*
-    表单提交，使用cmd测试
-    curl -v -X POST -d "test data" http://127.0.0.1:8080/submit
-    */
+    /**
+     * 表单提交，使用cmd测试
+     * curl -v -X POST -d "test data" http://127.0.0.1:8080/submit
+     */
     server.post("/submit", [](const net::HttpRequest& req) {
         net::HttpResponse resp;
         resp.status = net::HttpStatusCode::CREATED;
@@ -56,10 +56,10 @@ void example_http_server() {
         return resp;
     });
 
-    /*
-    重定向到 /new TODO 存在问题
-    http://127.0.0.1:8080/old
-    */
+    /**
+     * 重定向到 /new TODO 存在问题
+     * http://127.0.0.1:8080/old
+     */
     server.get("/old", [](const net::HttpRequest& req) {
         net::HttpResponse resp;
         resp.set_redirect("/new");
