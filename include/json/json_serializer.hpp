57f031c1b13a55b1c066d185e9cd4fb5c02de3d0
Ricky <w_ziwei2004@163.com>
2026-02-14 18:32:30 +0800

    refactor: json模块重构 | vscode支持优化

---
+++ include/json/json_serializer.hpp
@@ -0,0 +1,11 @@
+/**
+ * @brief 
+ * @author Ricky
+ * @date 2026/1/7
+ * @version 1.0
+ */
+
+#ifndef RICKY_CPP_JSON_SERIALIZER_HPP
+#define RICKY_CPP_JSON_SERIALIZER_HPP
+
+#endif // RICKY_CPP_JSON_SERIALIZER_HPP
---
+++ include/math/num_base.hpp
@@ -0,0 +1,11 @@
+/**
+ * @brief 
+ * @author Ricky
+ * @date 2026/1/18
+ * @version 1.0
+ */
+
+#ifndef RICKY_CPP_BASE_HPP
+#define RICKY_CPP_BASE_HPP
+
+#endif // RICKY_CPP_BASE_HPP
